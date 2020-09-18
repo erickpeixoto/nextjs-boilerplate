2c0dc4603d8be6322e99c92b042808fcbda311e6
Erick Eduardo <erickepeixoto@gmail.com>
2020-09-17 23:50:18 -0300

    chore: add support to eslint, prettier, husky and lintstaged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+const asss = 'teste'
+
 export default function Home() {
   return (
     <div className={styles.container}>
